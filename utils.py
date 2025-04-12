8c8109d1ff6bd1708d9069ff7666de2121b196b1
Kurt Wanner <kurt.wanner5625@gmail.com>
2025-04-12 16:13:49 -0400

    Worked on finetuning

--- utils.py
+++ utils.py
@@ -3,7 +3,19 @@ import random
 from tqdm import tqdm
 
 import torch
+from transformers import AutoTokenizer, AutoModel
 
+
+def load_model():
+    config = BertConfig.from_pretrained("zhihan1996/DNABERT-2-117M")
+    model = AutoModel.from_pretrained("zhihan1996/DNABERT-2-117M", trust_remote_code=True, config=config)
+
+
+    return model
+    
+def load_tokenizer():
+    tokenizer = AutoTokenizer.from_pretrained("zhihan1996/DNABERT-2-117M", trust_remote_code=True)
+    return tokenizer
 
 def set_random_seeds(seed_value=42):
     '''
